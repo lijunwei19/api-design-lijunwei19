334eada79133929f5dee22a1dc25e2221a6ceec2
Junwei LI <jlyc8@bu.edu>
2020-02-04 00:04:05 -0500

    update test.py and finish function

--- test_weatherAPI.py
+++ test_weatherAPI.py
@@ -1,1 +1,8 @@
+import pytest
 
+from weatherAPI import weather_data_download
+
+def test1():
+  assert weather_data_download((42.3656,71.0096),"us") == True
+  assert weather_data_download("London", "us") == True
+  assert weather_data_download(65201, "us") == True
--- weatherAPI.py
+++ weatherAPI.py
@@ -1,5 +1,7 @@
 import requests
 
+from pprintpp import pprint as pp
+
 def weather_data_download(id, country):
 
   if type(id) == str :
@@ -7,12 +9,15 @@ def weather_data_download(id, country):
     parameter = "q=" + id
 
   elif type (id) == tuple :
+
     i, j = id
+
     country=""
 
     parameter = "lat={}&lon={}".format(int(i),int(j))
 
   elif type(id) == int :
+    
     if len(str(id)) == 7 :
 
       parameter = "id=" + str(id)
@@ -20,22 +25,37 @@ def weather_data_download(id, country):
     elif len(str(id)) == 5:
 
       parameter = "zip=" + str(id)
+    
     else:
+    
       raise TypeError ("Please input valid int city name or zip or id")
   else:
+    
     raise TypeError ("Please input valid city name or zip or id")
 
   url = 'https://openweathermap.org/data/2.5/weather?{},{}&appid=b6907d289e10d714a6e88b30761fae22'.format(parameter, country)
 
-  print(url)
-
+  # print(url)
+ 
   Json_data = requests.get(url).json()
+  
+  pp (Json_data)
+  
+  if "message" in Json_data:
+  
+    return False
+  
+  else:
+  
+    return True 
 
-  print (Json_data)
 
 # def main():
-#   weather_data_download((42.3656,71.0096),"us")
+
+#   print(weather_data_download(65201, "us"))
+
 
 # if __name__ == '__main__':
+
 #   main()
-  
+#   
